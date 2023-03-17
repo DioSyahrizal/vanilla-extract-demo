454953d12185cdb54165283b026db508441dbb79
diosyahrizal <diosyahrizal@gmail.com>
2023-03-17 20:55:23 +0700

    fix: fix extension file import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from "next/app";
 
 import "@fontsource/poppins";
-import "@/styles/globals.css.ts";
-import "@/styles/normalize.css.ts";
+import "@/styles/globals.css";
+import "@/styles/normalize.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
